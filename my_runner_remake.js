a295b96bdbe40f36f616b5ad49b1c1c58eeb9d57
Moussa <moussa.fofana@epitech.eu>
2020-02-01 15:08:40 +0100

    added jump and scaling to the size of screen

--- my_runner_remake.js
+++ my_runner_remake.js
@@ -1,112 +1,5 @@
-// Player
-
-class Player {
-    constructor(texture) {
-        this.spr = new PIXI.Sprite(texture);
-        this.spr.anchor.x = 0.5;
-        this.spr.anchor.y = 1;
-        this.speedY = 0;
-    }
-    addToApp(app) {
-        app.stage.addChild(this.spr);
-    }
-    update() {
-        if (this.speedY > 5)
-            this.speedY = 3;
-        this.speedY += 0.1;
-        if (this.spr.position.y <= 216)
-            this.spr.position.y += this.speedY;
-    }
-}
-
-// Parallax manager
-class Plx_layer {
-    constructor(texture, speed) {
-        this.texture = texture;
-        this.speed = speed;
-        this.spr1 = new PIXI.Sprite(texture);
-        this.spr2 = new PIXI.Sprite(texture);
-    }
-    addToApp(app) {
-        app.stage.addChild(this.spr1);
-        app.stage.addChild(this.spr2);
-    }
-    update() {
-        this.spr1.position.x += this.speed;
-        this.spr2.position.x = this.spr1.position.x + this.spr1.width;
-        if (this.spr2.position.x < 0) {
-            let temp = this.spr2;
-            this.spr2 = this.spr1;
-            this.spr1 = temp;
-        }
-    }
-}
-
-class Parallax {
-    constructor(textures, initialspeed) {
-        this.layers = [];
-        for (i = 0; i < textures.length; i++) {
-            this.layers[i] = new Plx_layer(textures[i], initialspeed);
-            initialspeed -= 0.5;
-        }
-    }
-    addToApp(app) {
-        for (i = 0; i < this.layers.length; i++)
-            this.layers[i].addToApp(app);
-    }
-    update() {
-        for (i = 0; i < this.layers.length; i++)
-            this.layers[i].update();
-    }
-}
-
-//////////////////////
-
-
-// Main Part //
-
-// initialize pixi.js
-
-let bump = new Bump(PIXI);
-let app = new PIXI.Application({width: 384, height: 216});
-PIXI.settings.SPRITE_MAX_TEXTURES = Math.min(PIXI.settings.SPRITE_MAX_TEXTURES , 16);
-PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
-document.body.appendChild(app.view);
-app.renderer.view.style.position = "absolute";
-app.renderer.view.style.display = "block";
-
-
-// Texture loading
-
-let plx_textures = [];
-
-for (i = 1; i < 6; i ++)
-    plx_textures[i] = PIXI.Texture.from("ressources/Parallax/plx-" + i + ".png");
-
-let player_text = PIXI.Texture.from("ressources/Character/landing.png");
-
-
-let plx = new Parallax(plx_textures, 0.5);
-
-let player = new Player(player_text);
-
-
-let current_state = playState;
-
-plx.addToApp(app);
-player.addToApp(app);
-
-app.ticker.add(delta => gameLoop(delta));
-
-
-function gameLoop(delta) {
-    current_state(delta);
-}
-
-function playState(delta) {
-    plx.update();
-    player.update();
-}
+const logicalWidth = 384;
+const logicalHeight = 216;
 
 function keyboard(value) {
     let key = {};
@@ -154,3 +47,148 @@ function keyboard(value) {
     
     return key;
   }
+
+// Player
+
+class Player {
+    constructor(texture) {
+        this.spr = new PIXI.Sprite(texture);
+        this.spr.anchor.x = 0.5;
+        this.spr.anchor.y = 1;
+        this.spr.position.x = logicalWidth / 2;
+        this.speedY = 0;
+        this.isJumping = true;
+    }
+    addToApp(app) {
+        app.addChild(this.spr);
+    }
+    update() {
+        this.speedY += 0.5;
+        this.spr.position.y += this.speedY;
+        this.speedY *= 0.9;
+        if (this.spr.position.y > logicalHeight) {
+            this.spr.position.y = logicalHeight;
+            this.isJumping = false;
+            this.speedY = 0;
+        }
+    }
+    jump() {
+        if (this.isJumping == false) {
+            this.speedY = -10;
+            this.isJumping = true;
+        }
+    }
+}
+
+const resizeHandler = () => {
+    const scaleFactor = Math.min(
+      window.innerWidth / logicalWidth,
+      window.innerHeight / logicalHeight
+    );
+    const newWidth = Math.ceil(logicalWidth * scaleFactor);
+    const newHeight = Math.ceil(logicalHeight * scaleFactor);
+    
+    app.renderer.view.style.width = `${newWidth}px`;
+    app.renderer.view.style.height = `${newHeight}px`;
+  
+    app.renderer.resize(newWidth, newHeight);
+    mainContainer.scale.set(scaleFactor); 
+  };
+
+// Parallax manager
+class Plx_layer {
+    constructor(texture, speed) {
+        this.texture = texture;
+        this.speed = speed;
+        this.spr1 = new PIXI.Sprite(texture);
+        this.spr2 = new PIXI.Sprite(texture);
+    }
+    addToApp(app) {
+        app.addChild(this.spr1);
+        app.addChild(this.spr2);
+    }
+    update() {
+        this.spr1.position.x += this.speed;
+        this.spr2.position.x = this.spr1.position.x + this.spr1.width;
+        if (this.spr2.position.x < 0) {
+            let temp = this.spr2;
+            this.spr2 = this.spr1;
+            this.spr1 = temp;
+        }
+    }
+}
+
+class Parallax {
+    constructor(textures, initialspeed) {
+        this.layers = [];
+        for (i = 0; i < textures.length; i++) {
+            this.layers[i] = new Plx_layer(textures[i], initialspeed);
+            initialspeed -= 0.5;
+        }
+    }
+    addToApp(app) {
+        for (i = 0; i < this.layers.length; i++)
+            this.layers[i].addToApp(app);
+    }
+    update() {
+        for (i = 0; i < this.layers.length; i++)
+            this.layers[i].update();
+    }
+}
+
+//////////////////////
+
+
+// Main Part //
+
+// initialize pixi.js
+
+let mainContainer = new PIXI.Container();
+PIXI.settings.SPRITE_MAX_TEXTURES = Math.min(PIXI.settings.SPRITE_MAX_TEXTURES , 16);
+let bump = new Bump(PIXI);
+let app = new PIXI.Application({width: logicalWidth, height: logicalHeight});
+PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
+app.renderer.view.id = "pixi-canvas";
+document.body.appendChild(app.view);
+
+window.addEventListener('resize', resizeHandler, false);
+
+resizeHandler();
+// Texture loading
+
+let plx_textures = [];
+
+for (i = 1; i < 6; i ++)
+    plx_textures[i] = PIXI.Texture.from("ressources/Parallax/plx-" + i + ".png");
+
+let player_text = PIXI.Texture.from("ressources/Character/landing.png");
+
+
+let plx = new Parallax(plx_textures, 0.5);
+
+let player = new Player(player_text);
+
+
+let current_state = playState;
+
+let space_key = keyboard("ArrowUp");
+
+space_key.press = ()  => {
+    player.jump();
+}
+
+plx.addToApp(mainContainer);
+player.addToApp(mainContainer);
+app.stage.addChild(mainContainer);
+
+app.ticker.add(delta => gameLoop(delta));
+
+
+function gameLoop(delta) {
+    current_state(delta);
+}
+
+function playState(delta) {
+    plx.update();
+    player.update();
+}
