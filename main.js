42f91f51cea51d8aef488623c06402db6f107f3d
Moussa <moussa.fofana@epitech.eu>
2020-02-02 12:04:13 +0100

    now scale at the size of webpage and render pixelart style

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var game_config = {
+    type: Phaser.AUTO,
     width: 384,
     height: 216,
     backgroundColor: 0x000000,
@@ -8,8 +9,15 @@ var game_config = {
         arcade: {
             debug: false
         }
-    }
+    },
+    scale: {
+        mode: Phaser.Scale.FIT,
+        antialias: false
+    },
+    autoRound: false,
+    pixelArt: true
 }
 
 var game = new Phaser.Game(game_config);
+
 
